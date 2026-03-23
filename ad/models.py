9f02feb35682aeabad52e96f2b409463293e42f3
ilya <ilifud98@mail.ru>
2026-03-24 00:26:46 +0700

    refactor(ad.models): Task: add models.py in app ad

--- ad/models.py
+++ ad/models.py
@@ -5,10 +5,14 @@ from users.models import CustomUser
 class Ad(models.Model):
     title = models.CharField(max_length=100, verbose_name='Название объявления', help_text='Введите названия объявления')
     description = models.TextField(verbose_name='Описание объявления', help_text='Введите описание объявления')
-    image = models.ImageField(verbose_name='Фотография объявления')
-    date_created = models.DateTimeField(verbose_name='Дата создания объявления')
+    images = models.ImageField(verbose_name='Фотография объявления', null=True, blank=True)
+    date_created = models.DateTimeField(verbose_name='Дата создания объявления', auto_now=True)
     phone_number = models.IntegerField(verbose_name='Номер телефона')
     user = models.ForeignKey(CustomUser, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
 
     def __str__(self):
         return f'{self.title} - {self.description}'
