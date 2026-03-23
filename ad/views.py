5aacc75f31aa0be875cc4fbd0e8ba1cdf330eb4c
ilya <ilifud98@mail.ru>
2026-03-24 00:30:30 +0700

    refactor(ad.views): Task: add views.py in app ad

--- ad/views.py
+++ ad/views.py
@@ -1,41 +1,12 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
-from django.views.generic.edit import CreateView
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from ad.models import Ad
+from ad.serializers import AdSerializer
 
 
-class AdListView(ListView):
-    """Отбражение списка объявлений"""
+class AdViewSet(viewsets.ModelViewSet):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+    permission_classes = [AllowAny,]
 
-    model = Ad
-    template_name = 'ad/ad_list.html'
-    context_object_name = 'ad'
-
-class AdCreateView(CreateView):
-    """Создание объявления"""
-
-    model = Ad
-    template_name = 'ad/ad_form.html'
-    fields = ['__all__']
-    success_url = reverse_lazy("ad:ad_list")
-
-class AdDetailView(DetailView):
-    """Детали объявления"""
-
-    model = Ad
-    template_name = 'ad/ad_list.html'
-    context_object_name = 'ad'
-
-class AdUpdateView(UpdateView):
-    model = Ad
-    fields = ['__all__']
-
-    def get_success_url(self):
-        return reverse("ad:ad_detail", kwargs={"pk": self.object.pk})
-
-class AdDeleteView(DeleteView):
-    model = Ad
-    template_name = "ad/ad_confirm_delete.html"
-    success_url = reverse_lazy("ad:ad_list")
