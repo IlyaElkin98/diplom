c07b236f16d97736f20e9d7cad5aeb77b2bf23c8
ilya <ilifud98@mail.ru>
2026-03-24 00:31:02 +0700

    refactor(ad.urls): Task: add urls.py in app ad

--- ad/urls.py
+++ ad/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from ad.apps import AdConfig
+from ad.views import AdViewSet
 
 app_name = AdConfig.name
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'ad', AdViewSet)
 
+urlpatterns = [
+    path('', include(router.urls))
 ]
