aeb8e9eca80fd10c85fcbae7025191fca8549959
ilya <ilifud98@mail.ru>
2026-03-24 00:30:46 +0700

    refactor(ad.urls): Task: add urls.py in app ad

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from users.apps import UsersConfig
+from users.views import RegisterView, CustomTokenObtainPairView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('register/', RegisterView.as_view(), name='register'),
 ]
